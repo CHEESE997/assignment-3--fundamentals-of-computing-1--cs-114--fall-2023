89df3d803a24983b720cc0373b8570a8930863bf
CHEESE997 <143614071+CHEESE997@users.noreply.github.com>
2023-11-08 11:24:37 -0500

    fix Flipper

--- Matrix.java
+++ Matrix.java
@@ -2,63 +2,61 @@ public class Matrix {
   private int[][] matrix;
   private final int lengthOfMatrix;
 
-
   public Matrix(int size){
     matrix = new int[size][size];
     lengthOfMatrix = size;
+
   }
 
   public void printMatrix(){
-    //colors
-    String YELLOW = "\u001B[33m";
-    String RESET = "\u001B[0m";
-    //row
-      for(int i = 0; i<lengthOfMatrix; i++) {
-        //column
-       for(int j=0; j<lengthOfMatrix; j++) {
-        //if else to check for diagonal
-        // length-1-i to color the diagonal, first the last input should be colored then nextline is the index-i
-        if (j==lengthOfMatrix-1-i) {
-          System.out.print(YELLOW + matrix[i][j] + " " + RESET);
+    final String YELLOW = "\u001B[33m";
+    final String RESET = "\u001B[0m";
+
+    for(int row = 0; row<lengthOfMatrix; row++) {
+      for(int column=0; column<lengthOfMatrix; column++) {
+      //if else to check for diagonal
+      // length-1-i to color the diagonal, first the last input should be colored then nextline is the index-i
+        if (column==lengthOfMatrix-1-row) {
+          System.out.print(YELLOW + matrix[row][column] + " " + RESET);
+
         }else{
-          System.out.print(matrix[i][j] + " ");
+          System.out.print(matrix[row][column] + " ");
 
         }
-        }
-      System.out.println();
       }
+    System.out.println();
     }
+  }
 
   public void populateMatrix(){
-    //row
-    for(int i = 0; i<lengthOfMatrix; i++) {
-      //column
-      for(int j = 0; j<lengthOfMatrix; j++) {
-        //need to multiply length by index to count by intervals, the intervals being the length of the array, then add j for the numbers between the intervals, add 1 as index for arrays starts at 0
-         matrix[i][j] = (lengthOfMatrix*i+(1+j));
+    for(int row = 0; row<lengthOfMatrix; row++) {
+      for(int column = 0; column<lengthOfMatrix; column++) {
+        //need to multiply length by index to count by intervals, then add j for the numbers between the intervals, add 1 as index for arrays starts at 0
+         matrix[row][column] = (lengthOfMatrix*row+(1+column));
 
       }
     }
   }
 
   public void flipMatrix(){
-    //since index of items stat from 0, index of items will always be the length of the array minus 1, therefore when I need the index of an item i use lengthOfMatrix-1
+    //since index of items stat from 0, index of items will always be the length of the array minus 1, therefore use lengthOfMatrix-1 when dealing with index
     //need to run length-1 times to avoid last row
-    for(int i=0;i<lengthOfMatrix-1;i++){
+    for(int row=0;row<lengthOfMatrix-1;row++){
     //need to run length-1-i to decrement amount of times by 1 each time a row is completed to avoid the diagonals
-      for(int j=0;j<(lengthOfMatrix-1-i);j++){
-    //relationship between swapping on the diagonal is that the index of the desired number is the index of the last number minus the reversed index of number being swapped, I.E target index -> 2,1 is 2,2-0,1 which is on the opposite of the diagonal
-      swap(i, j, lengthOfMatrix-1-i, lengthOfMatrix-1-j);
+      for(int column=0;column<(lengthOfMatrix-1-row);column++){
+      swap(row, column, lengthOfMatrix-1-row, lengthOfMatrix-1-column);
+
       }
     }
   }
 
 
  private void swap (int x1,int y1,int x2,int y2) {
-    int[][] temparray = new int[1][1];
-    temparray[0][0] = matrix[x1][y1];
+    int temparray = matrix[x1][y1];
+
 
     matrix[x1][y1]= matrix[x2][y2];
-    matrix[x2][y2]= temparray[0][0];
+    matrix[x2][y2]= temparray;
+
   }
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -4,37 +4,38 @@ public class RunMatrix {
 
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
-    Matrix array;
+    Matrix matrix;
 
     System.out.print("Please enter a Nummber:");
 
-
     try {
       int input = Integer.parseInt(scan.nextLine());
+
       if (input<0){
         System.out.println("Cannot accept negative numbers");
         scan.close();
+
         return;
 
       }else{
-      array = new Matrix (input);
+      matrix = new Matrix (input);
       System.out.print("\nYour matrix is:" + (input) + " x ");
       // need to spit print statement inorder to assing type to print
       System.out.print((input) + "\n\nPrinting matrix with default matrix values\n");
-      array.printMatrix();
+      matrix.printMatrix();
       System.out.println("\nPopulating matrix... matrix populated" + "\n\nPrinting matrix");
-      array.populateMatrix();
-      array.printMatrix();
+      matrix.populateMatrix();
+      matrix.printMatrix();
       System.out.println("\nFlipping matrix...   matrix flipped" + "\n\nPrinting matrix");
-      array.flipMatrix();
-      array.printMatrix();
+      matrix.flipMatrix();
+      matrix.printMatrix();
 
       }
-    } catch (Exception e) {
+    }catch (Exception e){
       System.out.println("Input was not a integer " + e);
 
     }
-  scan.close();
+      scan.close();
 
   }
 }
