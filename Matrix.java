b8684a70ce9917ff3707373c87fa9b07536b8556
CHEESE997 <143614071+CHEESE997@users.noreply.github.com>
2023-11-08 01:27:55 -0500

    initial creation

--- Matrix.java
+++ Matrix.java
@@ -1,2 +1,11 @@
 public class Matrix {
+  private int[][] Matrix;
+  }
+
+  private void swap (int x1,int x2,int y1,int y2) {
+    int tempx = x1;
+    x1=y1;
+
+
+
 }
--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,26 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+    System.out.print("Please enter a Nummber:");
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Please enter a number");
+
+    try {
+      int input = Integer.parseInt(scan.nextLine());
+      if (input<0);{
+        System.out.println("Cannot accept negative numbers");
+        break;
+
+      } else{
+        Matrix()
+
+      }
+
+
+    } catch (Exception e) {
+      // TODO: handle exception
+      System.out.println("Input was not a number" + e);
+
+    }
   }
 }
